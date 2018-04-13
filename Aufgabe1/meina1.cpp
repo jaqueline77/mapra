54b49c05d78a861f7afaa177d0fb7fb6086ef9ef
Emma Ahrens <emmakatherina@icloud.com>
2018-04-13 13:35:09 +0200

    Initial commit containing all given files

---
+++ Aufgabe1/meina1.cpp
@@ -0,0 +1,23 @@
+/***********************************************************
+*  Name       : meina1.cpp                                 *
+*  Verwendung : Loesung der ersten Praktikumsaufgabe,      *
+*               Nullstellen eines quadratischen Polynoms   *
+*  Autor      :                                            *
+*  Datum      :                                            *
+*  Sprache    : C++                                        *
+***********************************************************/
+
+// Einbinden der Praktikums-Umgebung. Falls Sie die Ein-/Ausgabe zuerst
+// nicht ueber die Praktikumsumgebung machen wollen, sollten Sie auch noch
+// #include <iostream> einbinden.
+
+#include "unit.h"
+
+// ===== Hauptprogramm =====
+
+int main()
+{
+  // Hier kommt nun Ihr Programm; viel Erfolg!
+
+  return 0;
+}
